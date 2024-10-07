b6d99014af3ca65929687ab8543d1f7bdcf62a6a
ahmedRAOUANE <ahmedraouane30@gmail.com>
2024-10-07 09:08:54 +0100

    add custom style files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,64 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link";
+import { CiSearch } from "react-icons/ci";
+import { FaRegUserCircle } from "react-icons/fa";
+import { IoCartOutline } from "react-icons/io5";
+import { MdMenu } from "react-icons/md";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="container">
+      <header>
+        <nav className="box nowrap">
+          <div className="box nowrap">
+            <button className="hide-in-large">
+              <MdMenu />
+            </button>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <Link href={"/"}>
+              <h1>
+                SHOP.CO
+              </h1>
+            </Link>
+          </div>
+
+          <div className="box full-width nowrap">
+            <div className="box jc-start">
+              <select name="shop" id="shop" className="hide-in-small">
+                <option value="shop">Shop</option>
+              </select>
+
+              <ul className="box hide-in-small">
+                <li className="btn icon">
+                  <Link href={"#"}>On Sale</Link>
+                </li>
+                <li className="btn icon">
+                  <Link href={"#"}>New Arrivals</Link>
+                </li>
+                <li className="btn icon">
+                  <Link href={"#"}>Brands</Link>
+                </li>
+              </ul>
+            </div>
+
+            <div className="search-bar box jc-start full-width nowrap paper disable-guitters outline hide-in-small">
+              <CiSearch />
+              <input type="search" name="search" id="searchInput" className="transparent" placeholder="Search Products.." />
+            </div>
+
+            <div className="box nowrap">
+              <button className="hide-in-large icon">
+                <CiSearch />
+              </button>
+              <button className="icon">
+                <IoCartOutline />
+              </button>
+              <button className="icon">
+                <FaRegUserCircle />
+              </button>
+            </div>
+          </div>
+        </nav>
+      </header>
     </div>
   );
 }
